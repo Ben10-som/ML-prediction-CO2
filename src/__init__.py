6986e8ee481baf36c9615e71096b81f651141f03
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-12-26 19:26:48 +0000

    init hydra config

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""
+Seattle Energy ML 2016 - Package Principal
+Prédiction de la consommation énergétique des bâtiments de Seattle
+
+ENSAE - Machine Learning 1
+"""
+
+__version__ = "1.0.0"
+
+# Configuration du logging par défaut
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
---
+++ src/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+Utilitaires généraux du projet
+"""
+
+from .config_loader import load_config, get_paths
+
+__all__ = ['load_config', 'get_paths']
