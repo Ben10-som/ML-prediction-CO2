add5b969cc3dfb93a39bae903728715cd6890f5d
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-12-28 23:49:49 +0000

    docs(utils): ajout de commentaires dans src/utils/__init__.py pour clarifier son usage

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,7 +1,12 @@
 """
 Utilitaires généraux du projet
+Ce package regroupe des fonctions d'aide réutilisables
+pour la configuration et la gestion des chemins.
 """
 
 from .config_loader import load_config, get_paths
+# Cela permet de les rendre accessibles directement via src.utils
 
 __all__ = ['load_config', 'get_paths']
+# Définition de l'API publique du package utils
+# Seules ces fonctions seront exposées lors d'un import global
