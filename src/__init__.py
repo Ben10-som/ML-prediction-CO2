3f4e11ec5de176997a888a42241217c21ffe4491
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-12-28 23:38:45 +0000

    chore(src): reset de __init__.py
    Suppression de __version__ et du logging car la configuration est désormais centralisée via Hydra.
    
     On branch infra/init-config
     Changes to be committed:
    	modified:   src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -4,13 +4,3 @@ Prédiction de la consommation énergétique des bâtiments de Seattle
 
 ENSAE - Machine Learning 1
 """
-
-__version__ = "1.0.0"
-
-# Configuration du logging par défaut
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
